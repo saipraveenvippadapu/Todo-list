a783d48b8c43e4bc3fd970c80d29c1729b25f995
sai praveen <umasai215@gmail.com>
2024-04-10 17:08:34 +0530

    added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-
-// import App from "./App";
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-
-// root.render(<App />);
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
